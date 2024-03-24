e1bca8c19cbeb610dfbf2b179420604f926d3701
Michal Dubovsky <michal.dubovsky@cern.ch>
2024-03-24 14:52:33 +0100

    Use real data, not example log

--- python/app.py
+++ python/app.py
@@ -20,11 +20,11 @@ import time, datetime
 
 
 time_string = datetime.datetime.fromtimestamp(int(time.time())).strftime('%Y-%m-%d_%H:%M:%S')
-#log_address = "logs/log_" + time_string + ".txt"
-#power_supply = PowerSupplyControler(log_address)
-#power_supply.start_measurement(100,1000)
+log_address = "logs/log_" + time_string + ".txt"
+power_supply = PowerSupplyControler(log_address)
+power_supply.start_measurement(100,1000)
 
-log_address = "logs/log_test.txt"
+#log_address = "logs/log_test.txt"
 app = Bottle()
 
 @app.route('/')
